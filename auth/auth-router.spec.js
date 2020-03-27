3456d4bbc0bffab193b87fe9ce375552e39213f7
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-03-27 12:08:35 +0200

    wip

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -11,6 +11,7 @@ describe("register endpoint", () => {
       .post("/api/auth/register")
       .send({ username: "Dimos", password: "1234" })
       .then(res => {
+        // eslint-disable-next-line no-undef
         expect(res.status)
           .toBe(200)
           .expect("Content-Type", /json/);
